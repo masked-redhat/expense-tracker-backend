96c9032e8cef405ffd2dde4038c2881cead107a4
Zed <skhull.coding@proton.me>
2025-03-24 09:23:23 +0530

    chore: remove unneccessary endpoint

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { LoginRouter } from "./login.js";
 import { TransactionRouter } from "./transaction.js";
-import { loggedIn } from "../middlewares/auth/auth.js";
 import { AdminRouter } from "./admin.js";
 
 const router = Router();
@@ -13,10 +12,6 @@ router.use("/admin", AdminRouter);
 
 router.get("/", (_, res) => {
   res.send("hello");
-});
-
-router.get("/hh", loggedIn, (_, res) => {
-  res.send("oi");
 });
 
 export const AppRouter = router;
